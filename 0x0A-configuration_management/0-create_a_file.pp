a2260b9dfb523e664f6d5307302639417c196f71
The-alpha-male <ronnymunene@outlook.com>
2024-02-08 08:04:29 -0800

    uploaded files

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
-# create a file in /tmp
+# Using Puppet to create a file in /tmp.
+
 file { '/tmp/school':
-=======
-# Create a file
-file {'/tmp/school':
->>>>>>> 98026f0e6e1ff71b709cf6446b9306a23e1a89f6
-ensure  => present,
-mode    => '0744',
-content => 'I love Puppet',
-owner   => 'www-data',
-group   => 'www-data',
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
 }
